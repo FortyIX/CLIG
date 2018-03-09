1f44b4e0cdd7f01a5007f4417dd78a2978ad9e64
Fu Zhang <fuzhang.work@hotmail.com>
2018-03-09 02:25:56 +0000

    Make the file name more understandable

---
+++ src/Crawling.py
@@ -0,0 +1,86 @@
+
+
+from scrapy.crawler import CrawlerProcess
+from prettytable import PrettyTable
+import getpass
+import sys
+
+import Crawler
+
+#init the spider instance
+scanner=Crawler.Spider()
+
+
+
+Crawler.Spider.user=input("What is your K number ?")
+#get K number
+Crawler.Spider.passw=getpass.getpass()
+#get password
+
+
+
+#get crweler to work
+process = CrawlerProcess()
+process.crawl(scanner)
+process.start()
+
+
+
+
+# uncommont the following code to use the orginal output
+# Please only use the original output when prettyTable package is unavailable
+# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+# print("Course List")
+# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+# for i in scanner.getCourse():
+#     print("\b"+"\b"+ i )
+#     print("|++++++++++++++++++++++++++++++++++++++++++++++++++++++++|")
+#
+# print()
+# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+#
+# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+# print("Course List")
+# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+# for i in range(0,len(scanner.getGradeTitle())):
+#     print("\b"+"\b"+ scanner.getGradeTitle()[i]+"\b"+"\b"+scanner.getGradeMarks()[i])
+#     print("|++++++++++++++++++++++++++++++++++++++++++++++++++++++++|")
+#
+# print()
+# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+
+
+def printCourseTable():
+    course_table = PrettyTable(["No.", "Courses"])
+    for i in range(0, len(scanner.getCourse())):
+        course_table.add_row([i, scanner.getCourse()[i]])
+
+    print(course_table)
+    pass
+
+
+
+def printGradeTable():
+    course_table = PrettyTable(["Course", "Grade"])
+    for i in range(0, len(scanner.getCourse())):
+        course_table.add_row([scanner.getGradeTitle()[i], scanner.getGradeMarks()[i]])
+
+    print(course_table)
+    pass
+
+
+print("The Course you are doing ")
+printCourseTable()
+print("\n")
+print("and Your Grade")
+printGradeTable()
+
+
+
+
+
+
+
+
