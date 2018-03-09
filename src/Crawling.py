5e212c30de180d1819c6e5e9c3c0fb1c8f0f6743
Fu Zhang <contact@fzhang.co.uk>
2018-03-09 17:47:38 +0000

    Make the code to start crawl object-oriented

--- src/Crawling.py
+++ src/Crawling.py
@@ -7,129 +7,142 @@ import sys
 
 import Crawler
 
-#init the spider instance
-scanner=Crawler.Spider()
+
+class Crawling:
+    scanner = Crawler.Spider()
+    gradeDisable = False
+    safemode = False
+
+    def __init__(self):
+        # init the spider instance
+        self.scanner = Crawler.Spider()
+
+        # init setting
+        self.gradeDisable = True
+        self.safemode = False
 
 
-#init setting
-gradeDisable=True
-safemode=False
+
+
+
+    # print the course table
+    def printCourseTable(self):
+        course_table = PrettyTable(["No.", "Courses"])
+        for i in range(0, len(self.scanner.getCourse())):
+            course_table.add_row([i, self.scanner.getCourse()[i]])
+
+        print(course_table)
+        pass
+
+
+    # print the grade table
+    def printGradeTable(self):
+        course_table = PrettyTable(["Course", "Grade"])
+        for i in range(0, len(self.scanner.getCourse())):
+            course_table.add_row([self.scanner.getGradeTitle()[i], self.scanner.getGradeMarks()[i]])
+
+        print(course_table)
+        pass
+
+
+    def printResult(self):
+
+        # uncommont the following code to use the orginal output
+        # Please only use the original output when prettyTable package is unavailable
+        # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        # print("Course List")
+        # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        # for i in scanner.getCourse():
+        #     print("\b"+"\b"+ i )
+        #     print("|++++++++++++++++++++++++++++++++++++++++++++++++++++++++|")
+        #
+        # print()
+        # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+        #
+        # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        # print("Course List")
+        # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        # for i in range(0,len(scanner.getGradeTitle())):
+        #     print("\b"+"\b"+ scanner.getGradeTitle()[i]+"\b"+"\b"+scanner.getGradeMarks()[i])
+        #     print("|++++++++++++++++++++++++++++++++++++++++++++++++++++++++|")
+        #
+        # print()
+        # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+
+        # by default only print the course
+        print("The Course you are doing ")
+        self.printCourseTable()
+
+        print("\n")
+
+        if (self.gradeDisable == False):
+            print("and Your Grade")
+            self.printGradeTable()
+
+            pass
 
 
 
 
+    def parseArguments(self):
+        # Deal with the arguments
+        if (len(sys.argv) > 1):
+            if sys.argv[1] == "-g":
+                self.gradeDisable = False
+                if len(sys.argv) > 2:
+                    if sys.argv[2] == "-s":
+                        self.safemode = True
+                    else:
+                        self.safemode = False
+                else:
+                    self.safemode = False
 
-# Deal with the arguments
-if(len(sys.argv)>1):
-    if sys.argv[1] == "-g":
-        gradeDisable=False
-        if len(sys.argv) > 2:
-            if sys.argv[2]=="-s":
-                safemode=True
-            else:
-                safemode=False
+            elif sys.argv[1] == "-s":
+                self.safemode = True
+                if len(sys.argv) > 2:
+                    if sys.argv[2] == "-g":
+                        self.gradeDisable = False
+                    else:
+                        self.fgradeDisable = True
         else:
-            safemode=False
-
-    elif sys.argv[1] == "-s":
-        safemode=True
-        if len(sys.argv) > 2:
-            if sys.argv[2]=="-g":
-                gradeDisable=False
-            else:
-                gradeDisable=True
-else:
-    gradeDisable=True
-    safemode=False
-
-
-
-# get K number
-
-Crawler.Spider.user = input("What is your K number ?")
-
-
-#get password
-if safemode == True:
-    Crawler.Spider.passw = getpass.getpass()
-
-else:
-    Crawler.Spider.passw = input("What is your password ?")
+            self.gradeDisable = True
+            self.safemode = False
 
 
 
 
+    def getUserInfo(self):
+
+        # get K number
+        Crawler.Spider.user = input("What is your K number ?")
 
 
-#run the crawler
-process = CrawlerProcess()
-process.crawl(scanner)
-process.start()
+        self.parseArguments()
 
+        # get password
+        if self.safemode == True:
+            Crawler.Spider.passw = getpass.getpass()
 
-
-
-# uncommont the following code to use the orginal output
-# Please only use the original output when prettyTable package is unavailable
-# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-# print("Course List")
-# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-# for i in scanner.getCourse():
-#     print("\b"+"\b"+ i )
-#     print("|++++++++++++++++++++++++++++++++++++++++++++++++++++++++|")
-#
-# print()
-# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-
-#
-# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-# print("Course List")
-# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-# for i in range(0,len(scanner.getGradeTitle())):
-#     print("\b"+"\b"+ scanner.getGradeTitle()[i]+"\b"+"\b"+scanner.getGradeMarks()[i])
-#     print("|++++++++++++++++++++++++++++++++++++++++++++++++++++++++|")
-#
-# print()
-# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-
-
-# print the course table
-def printCourseTable():
-    course_table = PrettyTable(["No.", "Courses"])
-    for i in range(0, len(scanner.getCourse())):
-        course_table.add_row([i, scanner.getCourse()[i]])
-
-    print(course_table)
-    pass
-
-
-# print the grade table
-def printGradeTable():
-    course_table = PrettyTable(["Course", "Grade"])
-    for i in range(0, len(scanner.getCourse())):
-        course_table.add_row([scanner.getGradeTitle()[i], scanner.getGradeMarks()[i]])
-
-    print(course_table)
-    pass
+        else:
+            Crawler.Spider.passw = input("What is your password ?")
 
 
 
 
-# by default only print the course
-print("The Course you are doing ")
-printCourseTable()
-
-
-print("\n")
-
-
-if(gradeDisable==False):
-    print("and Your Grade")
-    printGradeTable()
+    def startCrawling(self):
+        # run the crawler
+        process = CrawlerProcess()
+        process.crawl(self.scanner)
+        process.start()
 
 
 
-
+    def crawling(self):
+        self.getUserInfo()
+        self.startCrawling()
+        self.printResult()
 
 
 
---
+++ src/launch.py
@@ -0,0 +1,10 @@
+from Crawling import  Crawling
+
+
+
+def main():
+    spider = Crawling()
+    spider.crawling()
+
+if __name__ == "__main__":
+    main()
